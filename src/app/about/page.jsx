5cc92c13fea6304cf98ba85d537bee087c1246fe
ibrahimkhalildev <ibrahimkholil01@gmail.com>
2026-09-22 08:12:32 +0600

    Image optimization

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import Image from 'next/image'
+import React from 'react'
 
 const AboutPage = () => {
-    return (
-        <div>
-            <h2>About Us</h2>
-        </div>
-    );
-};
+  return (
+    <div>
+      <h2>About Us</h2>
+      <Image src={'/WaterTank.jpg'} width={300} height='300' alt='LGED Water Header Tank'></Image>
+    </div>
+  )
+}
 
-export default AboutPage;
+export default AboutPage
